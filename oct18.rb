56e410b500f75bb99db53683b935d8efb21c78e5
Meera <mjvsharma@outlook.com>
2017-10-26 12:05:06 -0400

    final version

--- oct18.rb
+++ oct18.rb
@@ -1,26 +1,26 @@
-#write a method that accepts a number as an argument
-#and then it returns a string along with its ordinal indicator
-#between 1 and 20
-1st
-2nd
-3rd
-4th
-5th
-6th
-7th
-8th
-9th
-10th
-11th
-12th
-13th
-14th
-15th
-16th
-17th
-18th
-19th
-20th
+# #write a method that accepts a number as an argument
+# #and then it returns a string along with its ordinal indicator
+# #between 1 and 20
+# 1st
+# 2nd
+# 3rd
+# 4th
+# 5th
+# 6th
+# 7th
+# 8th
+# 9th
+# 10th
+# 11th
+# 12th
+# 13th
+# 14th
+# 15th
+# 16th
+# 17th
+# 18th
+# 19th
+# 20th
 
 
 def ordinal_indicator(number_between_1_20)
